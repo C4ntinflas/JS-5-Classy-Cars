a3f21d3c9c0d8daa0d77f3535383c606fc6de6cd
Jonathan <jonathanrodriguez069@gmail.com>
2023-09-09 14:05:25 -0400

    Passed

---
+++ index.js
@@ -0,0 +1,39 @@
+class Vehicle {
+    constructor(model){
+      this.model = model
+    }
+    drive(){
+      return this.model + " " + "drives"
+    }
+  }
+
+class Sedan extends Vehicle{
+    constructor(model, backupCamera){
+        super(model);
+        this.backupCamera = backupCamera;
+    }
+}
+
+class Suv extends Vehicle{
+    constructor(model, offroadPackage){
+        super(model);
+        this.offroadPackage = offroadPackage;
+    }
+}
+
+class Truck extends Vehicle{
+    constructor(model, towingCapacity){
+        super(model);
+        this.towingCapacity = towingCapacity;
+    }
+}
+
+class EVSedan extends Vehicle{
+    constructor(model, backupCamera){
+        super(model);
+        this.backupCamera = backupCamera;
+    }
+    recharge(){
+    return this.model + " " + "recharges"
+    }
+}
